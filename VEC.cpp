5d521702d01b5b49f60724aaa4296618fa4d1840
bridgelancer <thchoy@hotmail.com>
2017-09-02 06:08:07 +0800

    Can convert VAR to VEC matrices

--- VEC.cpp
+++ VEC.cpp
@@ -1,27 +1,31 @@
 #include <armadillo>
 
-using arma = armadillo;
+using namespace arma;
 
 arma::mat GetLagMatrix(arma::mat xMat, int nlags)
 {
-    arma::mat lag;
     int nrows = xMat.n_rows;
     int ncols = xMat.n_cols;
 
-    lag.zeros(nrows - nlags, ncols * nlags);
+    arma::mat lag = arma::mat(nrows - nlags, ncols * nlags);
 
     int counter = 1;
+    int counter2 = 0;
     // dunno whether deletes the last nlags row
 
     for (int i = 0; i < ncols * nlags; i++){
         for (int j = 0; j < nrows - nlags; j++){
-            double laggedValue = xMat(j + counter, i);
-            lag(j, i) = laggedValue;
-            }
-            if ( (i+1) % ncols == 0)
-                counter++;
+
+            lag(j, i) = xMat(j + counter, counter2);
+
         }
+            counter2++;
+            if ( (i+1) % ncols == 0){
+                counter++;
+                counter2 = 0;
+            }
     }
+
     return lag;
 }
 
@@ -39,20 +43,25 @@ arma::mat GetVECMPara(arma::mat regression)
                     if (i == j){
                         buffer = 1 + regression(j, i);
                     }
+
                     else if (i != j){
                         buffer = regression(j, i);
                     }
                     VEC(j, i) = buffer - VEC(j + ncols, i);
                 }
-                buffer = -regression(j ,i);
                 else if (j < nrows - ncols){
+                    buffer = -regression(j ,i);
                     VEC(j, i) = VEC(j + ncols, i) + buffer;
-                    printf("%f   ", tmp6(j , i));
                 }
-                else
-                    VEC(j , i) = buffer; 
+                else{
+                    buffer = -regression(j ,i);
+                    VEC(j , i) = buffer;
+                }
+                printf("%.4f   ", VEC(j , i));
         }
+        printf("\n\n\n");
     }
+    return VEC;
 }
 
 
@@ -79,8 +88,15 @@ arma::mat Regress(arma::mat lag, arma::mat xMat)
     //beta = ( design.t() * design ).i() * design.t() * observation; 
     arma::mat beta;
     solve(beta, lag.t() * lag ,  lag.t() * xMat);
+
     return beta;
 }
+
+void saveMatCSV(arma::mat Mat, std::string filename)
+{ 
+    Mat.save(filename, arma::csv_ascii);
+}
+
 
 int main()
 {
@@ -90,9 +106,15 @@ int main()
     arma::mat lag;
 
     xMat = loadCSV("GLD-GDX.csv");
+
     lag = GetLagMatrix(xMat, 15);
+    saveMatCSV(lag, "Lag.csv");
 
     xMat.shed_rows(45,59);
     regression = Regress(lag, xMat);
+    saveMatCSV(regression, "Regression.csv");
+
+    std::cout << "Testing \n";
     parameters = GetVECMPara(regression);
+    saveMatCSV(parameters, "VECM_Para.csv");
 }
